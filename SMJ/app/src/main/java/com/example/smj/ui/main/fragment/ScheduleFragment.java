256dfbb7974a79fae3598dcd830d783c40d62753
EvergreenTree97 <tkdfhr7497@naver.com>
2021-01-23 14:03:50 +0900

    feature #36 : calendarview demo

--- SMJ/app/src/main/java/com/example/smj/ui/main/fragment/ScheduleFragment.java
+++ SMJ/app/src/main/java/com/example/smj/ui/main/fragment/ScheduleFragment.java
@@ -1,6 +1,8 @@
 package com.example.smj.ui.main.fragment;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,9 +14,17 @@ import androidx.fragment.app.Fragment;
 
 import com.example.smj.R;
 import com.example.smj.ui.schedule.ScheduleAlarmlistPopupActivity;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class ScheduleFragment extends Fragment {
-    CalendarView calendarView;
+    MaterialCalendarView calendarView;
     public ScheduleFragment(){
 
     }
@@ -29,15 +39,34 @@ public class ScheduleFragment extends Fragment {
     }
     protected void init(View view){
         calendarView = view.findViewById(R.id.schedule_calendarView);
+        calendarView.setSelectedDate(CalendarDay.today());
+        Calendar startTimeCalendar = Calendar.getInstance();
+        Calendar endTimeCalendar = Calendar.getInstance();
+
+        int currentYear = startTimeCalendar.get(Calendar.YEAR);
+        int currentMonth = startTimeCalendar.get(Calendar.MONTH);
+        int currentDate = startTimeCalendar.get(Calendar.DATE);
+
+        endTimeCalendar.set(Calendar.MONTH, currentMonth+3);
+
+
+        calendarView.state().edit()
+                .setFirstDayOfWeek(Calendar.SUNDAY)
+                .setMinimumDate(CalendarDay.from(currentYear, currentMonth, 1))
+                .setMaximumDate(CalendarDay.from(currentYear, currentMonth+11, endTimeCalendar.getActualMaximum(Calendar.DAY_OF_MONTH)))
+                .setCalendarDisplayMode(CalendarMode.MONTHS)
+                .commit();
+
     }
-    protected void CalendarViewEvent(View v){
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+    protected void CalendarViewEvent(View v) {
+        calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
             @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
                 Intent intent = new Intent(getActivity(), ScheduleAlarmlistPopupActivity.class);
                 intent.putExtra("data", "Test Popup");
                 startActivityForResult(intent, 1);
             }
         });
     }
+
 }
