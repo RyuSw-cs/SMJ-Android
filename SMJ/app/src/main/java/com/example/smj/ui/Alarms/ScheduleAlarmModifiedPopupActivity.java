53ae59c860d66200446dad7d80e544e92f88f767
EvergreenTree97 <tkdfhr7497@naver.com>
2021-05-04 01:51:40 +0900

    feat #78 : 통신 전 삭제 수정 버튼 동작

--- SMJ/app/src/main/java/com/example/smj/ui/Alarms/ScheduleAlarmModifiedPopupActivity.java
+++ SMJ/app/src/main/java/com/example/smj/ui/Alarms/ScheduleAlarmModifiedPopupActivity.java
@@ -10,20 +10,25 @@ import android.widget.Button;
 import com.example.smj.R;
 
 public class ScheduleAlarmModifiedPopupActivity extends Activity {
-    private Button submit;
+    private Button submit, cancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_schedule_alarmmodified_popup);
+        init();
     }
     public void init(){
         submit = findViewById(R.id.schedule_alarmmodified_popup_modified);
+        cancel = findViewById(R.id.schedule_alarmmodified_popup_cancel);
         submit.setOnClickListener((view) ->{
             Intent intent = new Intent();
             intent.putExtra("result", "Close Popup");
             setResult(RESULT_OK, intent);
             finish();
+        });
+        cancel.setOnClickListener((view) ->{
+           finish();
         });
     }
     public void getData(){
