a2801eb9499d15a315901317f23c95c55c00e30c
EvergreenTree97 <tkdfhr7497@naver.com>
2021-05-25 20:05:09 +0900

    feat #97 : 알림_UI_수정

--- SMJ/app/src/main/java/com/example/smj/ui/Alarms/ScheduleAlarmModifiedPopupActivity.java
+++ SMJ/app/src/main/java/com/example/smj/ui/Alarms/ScheduleAlarmModifiedPopupActivity.java
@@ -3,22 +3,27 @@ package com.example.smj.ui.Alarms;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.smj.R;
 
 public class ScheduleAlarmModifiedPopupActivity extends Activity {
     private Button submit, cancel;
+    private TextView context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_schedule_alarmmodified_popup);
         init();
+        getData();
     }
     public void init(){
+        context = findViewById(R.id.schedule_alarmmodified_popup_modifiedquestion);
         submit = findViewById(R.id.schedule_alarmmodified_popup_modified);
         cancel = findViewById(R.id.schedule_alarmmodified_popup_cancel);
         submit.setOnClickListener((view) ->{
@@ -34,6 +39,9 @@ public class ScheduleAlarmModifiedPopupActivity extends Activity {
     public void getData(){
         Intent intent = getIntent();
         String data = intent.getStringExtra("data");
-        //main.setText(data);
+        if(data.equals("1")){
+            context.setText(R.string.schedule_submit);
+            submit.setText("등록");
+        }
     }
 }
